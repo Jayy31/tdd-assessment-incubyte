4873efd07822aed14c72bc27fd72f2b00ff7bc93
Jayy31 <jayrathod4235@gmail.com>
2023-08-25 19:45:54 +0530

    created moveForward() method in SpacecraftController

--- src/main/java/com/spaces/chandrayaan/SpacecraftController.java
+++ src/main/java/com/spaces/chandrayaan/SpacecraftController.java
@@ -53,7 +53,15 @@ public class SpacecraftController {
 	public void setDirection(char direction) {
 		this.direction = direction;
 	}
-
+	
+    private void moveForward() {
+        if (direction == 'N') y++;
+        else if (direction == 'S') y--;
+        else if (direction == 'E') x++;
+        else if (direction == 'W') x--;
+        else if (direction == 'U') z++;
+        else if (direction == 'D') z--;
+    }
 
 	public static void main(String[] args) {
         int startX = 0, startY = 0, startZ = 0;
@@ -61,10 +69,10 @@ public class SpacecraftController {
         char[] commands = {'f', 'r', 'u', 'b', 'l'};
 
         SpacecraftController spacecraft = new SpacecraftController(startX, startY, startZ, startDirection);
-
+        
     }
 
 	}
 	
 
-}
+
